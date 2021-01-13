7d247bd51c6af8b04c6c6b75fc52a096e28a38ac
idrewdragons <andrewcarey1111@gmail.com>
2021-01-13 16:27:14 -0500

    project init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import react, {Components} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className ="App-tittle"> Stringers Things </h1>
+        <button className ="App-buttons">Sign-up</button>
+        <button className ="App-buttons">Login</button>
+        <button className ="App-buttons">Logout</button>
       </header>
     </div>
   );
